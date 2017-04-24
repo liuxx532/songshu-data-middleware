490deba2e8a42d6cf7812a587bd8a5afd38459df
huanghaizhou <huanghaizhou@co-mall.com>
2017-04-24 18:10:04 +0800

    修改了退款sql

--- src/main/java/com/comall/songshu/repository/RefundRepository.java
+++ src/main/java/com/comall/songshu/repository/RefundRepository.java
@@ -20,21 +20,23 @@ public interface RefundRepository extends JpaRepository<Author,Long> {
 //    AND o."Channel" IN (0, 1, 2, 3, 5)
 
 
-    @Query(value = "SELECT sum(i.\"ActualRefundMoney\")\n" +
-        "FROM songshu_cs_refund_item i JOIN songshu_cs_payment_record r ON i.\"PaymentRecordId\" = r.\"Id\"\n" +
-        "  JOIN songshu_cs_order o ON r.\"MergePaymentNo\" = o.\"OrderNumber\"\n" +
-        "WHERE i.\"MoneyType\" = 1 AND i.\"RefundType\" = 1 AND i.\"Status\" = 5\n" +
-        "      AND o.\"OrderCreateTime\" BETWEEN ?1 AND ?2\n" +
-        "      AND o.\"Channel\" IN (0, 1, 2, 3, 5)", nativeQuery = true)
+    @Query(value = "SELECT SUM(item.\"ActualRefundMoney\") FROM songshu_cs_order oo " +
+        "INNER JOIN songshu_cs_order_payable op ON op.\"OrderId\" = oo.\"Id\" " +
+        "INNER JOIN songshu_cs_payment_record r ON op.\"MergePaymentId\" = r.\"MergePaymentNo\" " +
+        "INNER JOIN songshu_cs_refund_item item ON item.\"PaymentRecordId\" = r.\"Id\" " +
+        "WHERE r.\"PaymentModeType\" = 2 AND item. \"Status\" = 5 AND item.\"MoneyType\" = 1 " +
+        "AND item.\"RefundType\" = 1 AND oo.\"orderType\" IN(0, 1) AND item.\"LastModifyTime\" " +
+        "BETWEEN ?1 AND ?2 AND oo.\"Channel\" IN(0, 1, 2, 3, 5)", nativeQuery = true)
     Double getRefundWithAllPlatform(Timestamp beginTime, Timestamp endTime);
 
 
-    @Query(value = "SELECT sum(i.\"ActualRefundMoney\")\n" +
-        "FROM songshu_cs_refund_item i JOIN songshu_cs_payment_record r ON i.\"PaymentRecordId\" = r.\"Id\"\n" +
-        "  JOIN songshu_cs_order o ON r.\"MergePaymentNo\" = o.\"OrderNumber\"\n" +
-        "WHERE i.\"MoneyType\" = 1 AND i.\"RefundType\" = 1 AND i.\"Status\" = 5\n" +
-        "      AND o.\"OrderCreateTime\" BETWEEN ?2 AND ?3\n" +
-        "      AND o.\"Channel\" = ?1", nativeQuery = true)
+    @Query(value = "SELECT SUM(item.\"ActualRefundMoney\") FROM songshu_cs_order oo " +
+        "INNER JOIN songshu_cs_order_payable op ON op.\"OrderId\" = oo.\"Id\" " +
+        "INNER JOIN songshu_cs_payment_record r ON op.\"MergePaymentId\" = r.\"MergePaymentNo\" " +
+        "INNER JOIN songshu_cs_refund_item item ON item.\"PaymentRecordId\" = r.\"Id\" " +
+        "WHERE r.\"PaymentModeType\" = 2 AND item. \"Status\" = 5 AND item.\"MoneyType\" = 1 " +
+        "AND item.\"RefundType\" = 1 AND oo.\"orderType\" IN(0, 1) AND item.\"LastModifyTime\" " +
+        "BETWEEN ?2 AND ?3 AND oo.\"Channel\" = ?1", nativeQuery = true)
     Double getRefundWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime);
 
     // TODO and trend
