6b5da60fa8166cc8d7167d8391f3147680f17e73
liugaoyu <liugaoyu@co-mall.com>
2017-04-24 15:06:15 +0800

    targetMap message

--- src/main/java/com/comall/songshu/web/rest/util/TargetsMap.java
+++ src/main/java/com/comall/songshu/web/rest/util/TargetsMap.java
@@ -13,17 +13,17 @@ public class TargetsMap {
 
     public static Map<String,String> getTargets() {
         targets.put("Revenue", "销售额");
-        targets.put("Order","订单量");
+        targets.put("OrderCount","订单量");
         targets.put("AvgOrderRevenue", "客单价");
-        targets.put("Visitors", "活跃用户数");
+        targets.put("UniqueVisitors", "访客数");
         targets.put("Refund", "退款金额");
-        targets.put("GrossMargin", "毛利率");
+        targets.put("GrossMarginRate", "毛利率");
         targets.put("RevenueTrend", "销售额趋势图");
+        targets.put("OrderCountTrend", "订单量趋势图");
         targets.put("AvgOrderTrend", "客单价趋势图");
-        targets.put("OrderTrend", "订单量趋势图");
-        targets.put("VisitorsTrend", "活跃用户趋势图");
+        targets.put("UniqueVisitorsTrend", "访客数趋势图");
         targets.put("RefundTrend", "退款金额趋势图");
-        targets.put("GrossMarginTrend", "毛利趋势图");
+        targets.put("GrossMarginRateTrend", "毛利趋势图");
 //        targets.put("FinishedGroup","关闭团数");
 //        targets.put("ConsumerConversionRateTrend", "消费用户转化率趋势图");
 //        targets.put("AvgOrderRevenueTrend", "客单价趋势图");
