e991fe92077a8786bced2a528ce41b3825d45db4
wdc <wangduochao@co-mall.com>
2017-04-24 11:30:50 +0800

    Add AvgOrderRevenue service and repository

--- src/main/java/com/comall/songshu/repository/AvgOrderRevenueRepository.java
+++ src/main/java/com/comall/songshu/repository/AvgOrderRevenueRepository.java
@@ -2,11 +2,49 @@ package com.comall.songshu.repository;
 
 import com.comall.songshu.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.sql.Timestamp;
 
 /**
  * Created by wdc on 2017/4/19.
  */
 
-// TODO: Add AvgOrderRevenue entity
+// 客单价
 public interface AvgOrderRevenueRepository extends JpaRepository<Author, Long> {
+
+// SQL 说明
+//    @Query(value = "SELECT sum(r.\"Amount\") / count(DISTINCT o.\"Id\") as result \n" +
+//        "FROM songshu_cs_order o JOIN songshu_cs_payment_record r ON (o.\"OrderNumber\" = r.\"MergePaymentNo\")\n" +
+//        "  JOIN songshu_cs_order_payable p ON (o.\"Id\" = p.\"OrderId\")\n" +
+//        "WHERE (o.\"OrderStatus\" NOT IN (7, 8)) -- 排除取消，关闭\n" +
+//        "      AND (r.\"PaymentModeType\" = 2) -- 外部支付\n" +
+//        "      AND (r.\"PaidTime\" BETWEEN '2013-01-06 18:22:50' AND '2017-01-06 18:22:50')  -- 支付时间? 使用订单创建时间？\n" +
+//        "--       AND (o.OrderCreateTime BETWEEN '2013-01-06 18:22:50' AND '2017-01-06 18:22:50')\n" +
+//        "      AND (p.\"PaymentStatus\" = 1) -- 已支付\n" +
+//        "      AND (o.\"Channel\" IN (1, 2, 3, 5)); -- 销售渠道（1:安卓 2:IOS 3:微信 5:WAP", nativeQuery = true)
+
+    // 所有平台
+    @Query(value = "SELECT sum(r.\"Amount\") / count(DISTINCT o.\"Id\") as result \n" +
+        "FROM songshu_cs_order o JOIN songshu_cs_payment_record r ON (o.\"OrderNumber\" = r.\"MergePaymentNo\")\n" +
+        "  JOIN songshu_cs_order_payable p ON (o.\"Id\" = p.\"OrderId\")\n" +
+        "WHERE (o.\"OrderStatus\" NOT IN (7, 8))\n" +
+        "      AND (r.\"PaymentModeType\" = 2)\n" +
+        "      AND (r.\"PaidTime\" BETWEEN ?1 AND ?2)\n" +
+        "      AND (p.\"PaymentStatus\" = 1) \n" +
+        "      AND (o.\"Channel\" IN (0, 1, 2, 3, 5))", nativeQuery = true)
+    Double getAvgRevenueWithAllPlatform(Timestamp beginTime, Timestamp endTime);
+
+
+    // 单个平台
+    @Query(value = "SELECT sum(r.\"Amount\") / count(DISTINCT o.\"Id\") as result \n" +
+        "FROM songshu_cs_order o JOIN songshu_cs_payment_record r ON (o.\"OrderNumber\" = r.\"MergePaymentNo\")\n" +
+        "  JOIN songshu_cs_order_payable p ON (o.\"Id\" = p.\"OrderId\")\n" +
+        "WHERE (o.\"OrderStatus\" NOT IN (7, 8))\n" +
+        "      AND (r.\"PaymentModeType\" = 2)\n" +
+        "      AND (r.\"PaidTime\" BETWEEN ?2 AND ?3)\n" +
+        "      AND (p.\"PaymentStatus\" = 1) \n" +
+        "      AND (o.\"Channel\" = ?1)", nativeQuery = true)
+    Double getAvgRevenueWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime);
+
 }
--- src/main/java/com/comall/songshu/service/AvgOrderRevenueService.java
+++ src/main/java/com/comall/songshu/service/AvgOrderRevenueService.java
@@ -4,6 +4,8 @@ import com.comall.songshu.repository.AvgOrderRevenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+
 /**
  * Created by wdc on 2017/4/19.
  */
@@ -17,5 +19,15 @@ public class AvgOrderRevenueService {
         return  avgOrderRevenueRepository.findAll().toArray();
     }
 
+    public Double getAvgRevenueWithAllPlatform(Timestamp beginTime, Timestamp endTime) {
+        return avgOrderRevenueRepository.getAvgRevenueWithAllPlatform(beginTime, endTime);
+    }
+
+
+    public Double getAvgRevenueWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime) {
+        return avgOrderRevenueRepository.getAvgRevenueWithSinglePlatform(platform, beginTime, endTime);
+    }
+
+    // TODO add trend
 
 }
