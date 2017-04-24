189058089fbc47e6d779e6b703acc2a338cc8f13
liugaoyu <liugaoyu@co-mall.com>
2017-04-24 15:30:04 +0800

    transutil message

--- src/main/java/com/comall/songshu/web/rest/util/TransferUtil.java
+++ src/main/java/com/comall/songshu/web/rest/util/TransferUtil.java
@@ -1,30 +1,38 @@
 package com.comall.songshu.web.rest.util;
 
+import java.sql.Timestamp;
+
 /**
  * Created by lgx on 17/4/11.
  */
 public  class TransferUtil {
 
-    public static int getGroupType(String pshGroupType){
-        if(pshGroupType.equals("singleGroup")) {
-            return 1;
-        } else if(pshGroupType.equals("multiGroup")) {
-            return 0;
-        }
+    /**
+     *
+     * @param platName
+     * @return
+     *
+     * 前台：微信，安卓，IOS，其他，WAP，全部
+     * 数据库：如果是4代表其他，-1代表全部
+     */
+  public static Integer getPlatform(String platName){
+        switch (platName){
+            case "wechat"://微信
+                return 3;
+            case "andriod":
+                return 1;
+            case "ios":
+                return 2;
+            case "others":
+                return 4 ;
+            case "wap":
+                return 5;
+            case "all":
+                return 0;
+            default:
+                return -1;//异常
 
-        return -1;
-    }
+        }
+  }
 
-    public static String getMidName(String mid){
-        String midName = null;
-        if(mid.equals("east")) {
-            midName = "华东站";
-        } else if (mid.equals("south")) {
-            midName="华南站";
-        }
-        else if(mid.equals("north")) {
-            midName="华北站";
-        }
-        return midName;
-    }
 }
