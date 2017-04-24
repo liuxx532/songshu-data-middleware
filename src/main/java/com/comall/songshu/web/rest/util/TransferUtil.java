d1eb5cfcd201c54fd5330e3d1b9801b12e6bb06e
liushengling <liushengling@co-mall.com>
2017-04-24 15:51:40 +0800

    targets Util类提交

--- src/main/java/com/comall/songshu/web/rest/util/TransferUtil.java
+++ src/main/java/com/comall/songshu/web/rest/util/TransferUtil.java
@@ -13,26 +13,52 @@ public  class TransferUtil {
      * @return
      *
      * 前台：微信，安卓，IOS，其他，WAP，全部
-     * 数据库：如果是4代表其他，-1代表全部
+     * 1:安卓 2:IOS 3:微信 5:WAP 0:其他 -1:全部
      */
   public static Integer getPlatform(String platName){
         switch (platName){
-            case "wechat"://微信
-                return 3;
             case "andriod":
                 return 1;
             case "ios":
                 return 2;
-            case "others":
-                return 4 ;
+            case "wechat":
+                return 3;
             case "wap":
                 return 5;
-            case "all":
-                return 0;
+            case "others":
+                return 0 ;
             default:
-                return -1;//异常
+                return -1;
 
         }
+
   }
+
+    /**
+     *
+     * @param channelId
+     * @return
+     *
+     * 前台：微信，安卓，IOS，其他，WAP，全部
+     * 1:安卓 2:IOS 3:微信 5:WAP 0:其他 -1:全部
+     */
+    public static String getPlatformName(Integer channelId){
+        switch (channelId){
+            case 1:
+                return "andriod";
+            case 2:
+                return "ios";
+            case 3:
+                return "wechat";
+            case 5:
+                return "wap";
+            case 0:
+                return "others";
+            default:
+                return "";
+
+        }
+
+    }
 
 }
