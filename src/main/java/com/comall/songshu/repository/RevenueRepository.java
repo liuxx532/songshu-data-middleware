9ab327278042b36b857781b3380200ad113b112a
wdc <wangduochao@co-mall.com>
2017-04-25 18:28:56 +0800

    Add RevenueTrend

--- src/main/java/com/comall/songshu/repository/RevenueRepository.java
+++ src/main/java/com/comall/songshu/repository/RevenueRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Timestamp;
-
+import java.util.List;
 
 
 /**
@@ -50,6 +50,41 @@ public interface RevenueRepository extends JpaRepository<Author, Long> {
 
 
     // TODO trend
+
+    // 所有平台趋势
+    @Query(value = "SELECT tss.stime as stime, tss.etime as etime, sum(r.\"Amount\")\n" +
+        "FROM songshu_cs_order o JOIN songshu_cs_payment_record r ON o.\"OrderNumber\" = r.\"MergePaymentNo\"\n" +
+        "  JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\"\n" +
+        "  JOIN\n" +
+        "  (SELECT ts.generate_series as stime, ts.generate_series + ?3 * interval '1 second' as etime\n" +
+        "   FROM (select generate_series(?1, ?2, ?3 * interval '1 second')) ts) tss\n" +
+        "    on (o.\"OrderCreateTime\" < tss.etime AND o.\"OrderCreateTime\" >= tss.stime)\n" +
+        "WHERE (o.\"OrderStatus\" not IN (6, 7))\n" +
+        "      AND (r.\"PaymentModeType\" = 2)\n" +
+        "      AND (r.\"PaidTime\" BETWEEN ?1 AND ?2)\n" +
+        "      AND (p.\"PaymentStatus\" = 1)\n" +
+        "      AND (o.\"Channel\" IN (0, 1, 2, 3, 5))\n" +
+        "GROUP BY tss.stime, tss.etime\n" +
+        "ORDER BY tss.stime", nativeQuery = true)
+    List<Object[]> getRevenueTrendWithAllPlatform(Timestamp beginTime, Timestamp endTime, Integer interval);
+
+
+    // 单个平台趋势
+    @Query(value = "SELECT tss.stime as stime, tss.etime as etime, sum(r.\"Amount\")\n" +
+        "FROM songshu_cs_order o JOIN songshu_cs_payment_record r ON o.\"OrderNumber\" = r.\"MergePaymentNo\"\n" +
+        "  JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\"\n" +
+        "  JOIN\n" +
+        "  (SELECT ts.generate_series as stime, ts.generate_series + ?4 * interval '1 second' as etime\n" +
+        "   FROM (select generate_series(?2, ?3, ?4 * interval '1 second')) ts) tss\n" +
+        "    on (o.\"OrderCreateTime\" < tss.etime AND o.\"OrderCreateTime\" >= tss.stime)\n" +
+        "WHERE (o.\"OrderStatus\" not IN (6, 7))\n" +
+        "      AND (r.\"PaymentModeType\" = 2)\n" +
+        "      AND (r.\"PaidTime\" BETWEEN ?2 AND ?3)\n" +
+        "      AND (p.\"PaymentStatus\" = 1)\n" +
+        "      AND (o.\"Channel\" = ?1)\n"+
+        "GROUP BY tss.stime, tss.etime\n" +
+        "ORDER BY tss.stime", nativeQuery = true)
+    List<Object[]> getRevenueTrendWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime, Integer interval);
 
 
 }
