8cf12a0c150aa99559dabeab3b739b0b7307da74
liushengling <liushengling@co-mall.com>
2017-04-25 11:08:27 +0800

    temp

--- src/main/java/com/comall/songshu/web/rest/IndexResource.java
+++ src/main/java/com/comall/songshu/web/rest/IndexResource.java
@@ -4,10 +4,12 @@ import com.codahale.metrics.annotation.Timed;
 import com.comall.songshu.repository.AuthorRepository;
 import com.comall.songshu.service.*;
 import com.comall.songshu.web.rest.util.TargetsMap;
+import jdk.nashorn.internal.runtime.options.Option;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Controller for view and managing Log Level at runtime.
@@ -104,96 +107,77 @@ public class IndexResource {
                                     @RequestBody String requestBody) throws Exception{
 
         log.debug("[RequestBody] {}", requestBody);
-
-        JSONObject obj = new JSONObject(requestBody);
-
         DateTimeFormatter dateTimeFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
-        JSONObject range = (JSONObject)obj.get("range");
 
 
-        Timestamp fromTime = Timestamp.valueOf(new DateTime(range.get("from")).toString(dateTimeFormat));
-        Timestamp toTime = Timestamp.valueOf(new DateTime(range.get("to")).toString(dateTimeFormat));
-        JSONArray targets = (JSONArray)obj.get("targets");
+        if(Optional.ofNullable(requestBody).isPresent()){
 
-        String target = "";
-        if (targets.length() > 0) {
-            JSONObject targetObj = (JSONObject)targets.get(0);
-            //获取指标中文名称
-            String targetName = targetObj.get("target").toString();
-            //获取指标英文名称
-            target = TargetsMap.getTargets().get(targetName);
-        }
+            JSONObject obj = new JSONObject(requestBody);
 
-        String platform = obj.get("platform").toString();
-
-
-//        if (target.endsWith("Trend")){
-//
-//            switch (target){
-//                case "RevenueTrend" :
-//
-//                    break;
-//
-//                case "OrderTrend" :
-//
-//                    break;
-//                case "AvgOrderRevenueTrend" :
-//
-//                    break;
-//                case "VisitorsTrend" :
-//
-//                    break;
-//                case "RefundTrend" :
-//
-//                    break;
-//                case "GrossMarginTrend" :
-//
-//                    break;
-//                case "NewSubscribersTrend" :
-//
-//                    break;
-//                case "FirstOrderTrend" :
-//
-//                    break;
-//                case "NotFirstOrderTrend" :
-//
-//                    break;
-//                default:
-//                    throw new IllegalArgumentException("target Trend"+target);
-//            }
-
-      //  }else {
-            switch (target) {
-
-                case "Revenue":
-                    return revenueService.getRevenue(target,platform,range.getString("from"),range.getString("to"));
-                case "OrderCount":
-                    // return orderService.getOrder();
-                case "AvgOrderRevenue":
-                      return avgOrderRevenueService.getAvgOrderRevenue(platform,range.getString("from"),range.getString("to"));
-                case "UniqueVisitors":
-                    //   return visitorsService.getVisitors();
-                case "Refund":
-                    //   return refundService.getRefund();
-                case "GrossMarginRate":
-                    //  return grossMarginService.getGrossMargin();
-                default:
-                    throw new IllegalArgumentException("target=" + target);
-
-
+            JSONObject range = (JSONObject)obj.get("range");
+            //开始时间
+            String fromTime = null;
+            //结束时间
+            String toTime = null;
+            if(Optional.ofNullable(range).isPresent()){
+                Object fromObj = range.get("from");
+                Object toObj = range.get("to");
+                fromTime = Optional.ofNullable(fromObj)
+                    .map( o -> (String)fromObj)
+                    .orElse(null);
+                toTime = Optional.ofNullable(toObj)
+                    .map( o -> (String)toObj)
+                    .orElse(null);
             }
-       // }
+
+            //指标中文名称
+            JSONArray targets = (JSONArray)obj.get("targets");
+            String target = null;
+            if (Optional.ofNullable(targets)
+                .filter((value) -> value.length() >0)
+                .isPresent()) {
+                JSONObject targetJsonObj = (JSONObject)targets.get(0);
+                if (Optional.ofNullable(targetJsonObj).isPresent()){
+                    Object targetObj =  targetJsonObj.get("target");
+                    target = Optional.ofNullable(targetObj)
+                        .map( o -> o.toString())
+                        .map( s -> TargetsMap.getTargets().get(s))
+                        .orElse(null);
+                }
+            }
+
+            //指定平台（渠道）
+            Object platformObj =  obj.get("platform");
+            String platform = Optional.ofNullable(platformObj)
+                .map( o -> o.toString())
+                .orElse(null);
 
 
+            if (Optional.ofNullable(fromTime).isPresent()
+                && Optional.ofNullable(toTime).isPresent()
+                && Optional.ofNullable(target).isPresent()
+                && Optional.ofNullable(platform).isPresent()){
+                switch (target) {
+                        case "Revenue":
+                            return revenueService.getRevenue(target,platform,fromTime,toTime);
+                        case "OrderCount":
+                            // return orderService.getOrder();
+                        case "AvgOrderRevenue":
+                            return avgOrderRevenueService.getAvgOrderRevenue(platform,range.getString("from"),range.getString("to"));
+                        case "UniqueVisitors":
+                            //   return visitorsService.getVisitors();
+                        case "Refund":
+                            //   return refundService.getRefund();
+                        case "GrossMarginRate":
+                            //  return grossMarginService.getGrossMargin();
+                        default:
+                            throw new IllegalArgumentException("target=" + target);
 
+                }
+            }
 
-
-
-
-
-
-
-       // return null;
+        }
+        return null;
     }
 
 }
