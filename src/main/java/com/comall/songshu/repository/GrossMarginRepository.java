e92b8a07e3420f63b86a66b0f29eed388015acc3
huanghaizhou <huanghaizhou@co-mall.com>
2017-04-26 10:56:26 +0800

    调整毛利率sql

--- src/main/java/com/comall/songshu/repository/GrossMarginRepository.java
+++ src/main/java/com/comall/songshu/repository/GrossMarginRepository.java
@@ -33,8 +33,6 @@ public interface GrossMarginRepository extends JpaRepository<Author,Long> {
         "INNER JOIN songshu_cs_order_item coi ON coo.\"Id\" = coi.\"OrderId\" " +
         "INNER JOIN songshu_cs_goods cg ON cg.\"Id\" = coi.\"GoodsId\"", nativeQuery = true)
     Double getProductCostWithAllPlatform(Timestamp beginTime, Timestamp endTime);
-
-
     @Query(value = "SELECT SUM(cg.\"CostPrice\" * coi.\"Quantity\") " +
         "FROM( SELECT DISTINCT co.\"Id\" FROM( SELECT * FROM songshu_cs_payment_record " +
         "WHERE \"PaymentModeType\"= 2 AND \"PaidTime\" BETWEEN ?2 AND ?3)cpr " +
@@ -44,6 +42,27 @@ public interface GrossMarginRepository extends JpaRepository<Author,Long> {
         "INNER JOIN songshu_cs_order_item coi ON coo.\"Id\" = coi.\"OrderId\" " +
         "INNER JOIN songshu_cs_goods cg ON cg.\"Id\" = coi.\"GoodsId\"", nativeQuery = true)
     Double getProductCostWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime);
+
+//SQL:
+    //SELECT( grossmargin.AfterFoldingPrice - grossmargin.referCost)/ grossmargin.AfterFoldingPrice AS goodsGrossMargin
+    // FROM ( SELECT SUM(coi."Quantity" * coi."ReferCost")AS referCost, SUM(coi."AfterFoldingPrice")AS AfterFoldingPrice
+    // FROM songshu_cs_order co RIGHT JOIN ( SELECT DISTINCT oo."Id" FROM songshu_cs_order oo
+    // INNER JOIN songshu_cs_order_payable cop ON oo."Id" = cop."OrderId"
+    // INNER JOIN songshu_cs_payment_record cpr ON cpr."MergePaymentNo" = cop."MergePaymentId" WHERE cop."PaymentStatus" = 1
+    // AND cpr."PaymentModeType" = 2 AND cpr."PaidTime" BETWEEN '2015-09-01 00:00:00' AND '2016-01-09 00:00:00' AND oo."OrderStatus"
+    // NOT IN (6,7) AND oo."orderType" IN(0, 1) AND oo."Channel" IN(0, 1, 2, 3, 5) ) coo ON co."Id" = coo."Id"
+    // INNER JOIN songshu_cs_order_item coi ON co."Id" = coi."OrderId" )grossmargin
+
+    @Query(value = "SELECT( grossmargin.AfterFoldingPrice - grossmargin.referCost)/ grossmargin.AfterFoldingPrice AS goodsGrossMargin " +
+        "FROM ( SELECT SUM(coi.\"Quantity\" * coi.\"ReferCost\")AS referCost, SUM(coi.\"AfterFoldingPrice\")AS AfterFoldingPrice " +
+        "FROM songshu_cs_order co RIGHT JOIN ( SELECT DISTINCT oo.\"Id\" FROM songshu_cs_order oo " +
+        "INNER JOIN songshu_cs_order_payable cop ON oo.\"Id\" = cop.\"OrderId\" " +
+        "INNER JOIN songshu_cs_payment_record cpr ON cpr.\"MergePaymentNo\" = cop.\"MergePaymentId\" " +
+        "WHERE cop.\"PaymentStatus\" = 1 AND cpr.\"PaymentModeType\" = 2 AND cpr.\"PaidTime\" " +
+        "BETWEEN ?1 AND ?2 AND oo.\"OrderStatus\" NOT IN (6,7) AND oo.\"orderType\" IN(0, 1) " +
+        "AND oo.\"Channel\" IN(0, 1, 2, 3, 5) ) coo ON co.\"Id\" = coo.\"Id\" " +
+        "INNER JOIN songshu_cs_order_item coi ON co.\"Id\" = coi.\"OrderId\" )grossmargin", nativeQuery = true)
+    Double getCrossMarginWithAllPlatform(Timestamp beginTime, Timestamp endTime);
 
     // TODO add trend
     // TODO fix slow sql query
