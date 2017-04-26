03722d5cf55a41e156906e90628c1f20b571cf0b
huanghaizhou <huanghaizhou@co-mall.com>
2017-04-26 19:08:40 +0800

    修改客单价趋势SQL

--- src/main/java/com/comall/songshu/repository/AvgOrderRevenueRepository.java
+++ src/main/java/com/comall/songshu/repository/AvgOrderRevenueRepository.java
@@ -50,40 +50,37 @@ public interface AvgOrderRevenueRepository extends JpaRepository<Author, Long> {
         "      AND (o.\"Channel\" = ?1)", nativeQuery = true)
     Double getAvgRevenueWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime);
 
+//SQL
+    //SELECT tss.stime AS stime, tss.etime AS etime, sum(comt.Amount) / count(DISTINCT comt.Id) AS result
+    // FROM(select r."Amount" as Amount ,r."PaidTime" as PaidTime,o."Id" as Id from songshu_cs_order o
+    // JOIN songshu_cs_payment_record r ON o."OrderNumber" = r."MergePaymentNo"
+    // JOIN songshu_cs_order_payable p ON o."Id" = p."OrderId" WHERE (o."OrderStatus" NOT IN (6, 7)) AND (r."PaymentModeType" = 2)
+    // AND (r."PaidTime" BETWEEN '2015-11-01 00:00:00' AND '2016-01-01 00:00:00') AND (p."PaymentStatus" = 1) AND (o."Channel" = 0)) comt
+    // RIGHT JOIN (SELECT ts.generate_series AS stime, ts.generate_series + 86400 * INTERVAL '1 second' AS etime
+    // FROM (SELECT generate_series('2015-11-01 00:00:00', '2016-01-01 00:00:00', 86400 * INTERVAL '1 second')) ts) tss
+    // ON (comt.PaidTime < tss.etime AND comt.PaidTime >= tss.stime) GROUP BY tss.stime, tss.etime ORDER BY tss.stime
+
     // 单个平台的趋势
-    @Query(value = "SELECT tss.stime as stime, tss.etime as etime, sum(r.\"Amount\") as amount\n" +
-        "FROM songshu_cs_order o JOIN songshu_cs_payment_record r ON o.\"OrderNumber\" = r.\"MergePaymentNo\"\n" +
-        "  JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\"\n" +
-        "  JOIN\n" +
-        "  (SELECT ts.generate_series as stime, ts.generate_series + ?4 * interval '1 second' as etime\n" +
-        "   FROM (select generate_series(?2, ?3, ?4 * interval '1 second')) ts) tss\n" +
-        "WHERE\n" +
-        "  (o.\"OrderStatus\" not IN (6, 7))\n" +
-        "  AND (r.\"PaymentModeType\" = 2)\n" +
-        "  AND (r.\"PaidTime\" BETWEEN ?2 AND ?3)\n" +
-        "  AND (p.\"PaymentStatus\" = 1)\n" +
-        "  AND (o.\"Channel\" = ?1)\n" +
-        "GROUP BY tss.stime, tss.etime\n" +
-        "ORDER BY tss.stime", nativeQuery = true)
+    @Query(value = "SELECT tss.stime AS stime, tss.etime AS etime, COALESCE((sum(comt.Amount) / count(DISTINCT comt.Id)),0) AS result " +
+        "FROM(select r.\"Amount\" as Amount ,r.\"PaidTime\" as PaidTime,o.\"Id\" as Id from songshu_cs_order o " +
+        "JOIN songshu_cs_payment_record r ON o.\"OrderNumber\" = r.\"MergePaymentNo\" JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\" " +
+        "WHERE (o.\"OrderStatus\" NOT IN (6, 7)) AND (r.\"PaymentModeType\" = 2) AND (r.\"PaidTime\" " +
+        "BETWEEN ?2 AND ?3) AND (p.\"PaymentStatus\" = 1) AND (o.\"Channel\" = ?1)) comt " +
+        "RIGHT JOIN (SELECT ts.generate_series AS stime, ts.generate_series + ?4 * INTERVAL '1 second' AS etime " +
+        "FROM (SELECT generate_series(?2, ?3, ?4 * INTERVAL '1 second')) ts) tss " +
+        "ON (comt.PaidTime < tss.etime AND comt.PaidTime >= tss.stime) GROUP BY tss.stime, tss.etime ORDER BY tss.stime", nativeQuery = true)
     List<Object[]> getAvgRevenueTrendWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime, Integer interval);
 
 
     // 所有平台的趋势
-    @Query(value = "SELECT tss.stime as stime, tss.etime as etime, sum(r.\"Amount\") as amount\n" +
-        "FROM songshu_cs_order o JOIN songshu_cs_payment_record r ON o.\"OrderNumber\" = r.\"MergePaymentNo\"\n" +
-        "  JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\"\n" +
-        "  JOIN\n" +
-        "  (SELECT ts.generate_series as stime, ts.generate_series + ?3 * interval '1 second' as etime\n" +
-        "   FROM (select generate_series(?1, ?2, ?3 * interval '1 second')) ts) tss\n" +
-        "    on (o.\"OrderCreateTime\" < tss.etime AND o.\"OrderCreateTime\" >= tss.stime)\n" +
-        "WHERE\n" +
-        "  (o.\"OrderStatus\" not IN (6, 7))\n" +
-        "  AND (r.\"PaymentModeType\" = 2)\n" +
-        "  AND (r.\"PaidTime\" BETWEEN ?1 AND ?2)\n" +
-        "  AND (p.\"PaymentStatus\" = 1)\n" +
-        "  AND (o.\"Channel\" IN (0, 1, 2, 3, 5))\n" +
-        "GROUP BY tss.stime, tss.etime\n" +
-        "ORDER BY tss.stime", nativeQuery = true)
+    @Query(value = "SELECT tss.stime AS stime, tss.etime AS etime, COALESCE((sum(comt.Amount) / count(DISTINCT comt.Id)),0) AS result " +
+        "FROM(select r.\"Amount\" as Amount ,r.\"PaidTime\" as PaidTime,o.\"Id\" as Id from songshu_cs_order o " +
+        "JOIN songshu_cs_payment_record r ON o.\"OrderNumber\" = r.\"MergePaymentNo\" JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\" " +
+        "WHERE (o.\"OrderStatus\" NOT IN (6, 7)) AND (r.\"PaymentModeType\" = 2) AND (r.\"PaidTime\" " +
+        "BETWEEN ?1 AND ?2) AND (p.\"PaymentStatus\" = 1) AND (o.\"Channel\" IN (0, 1, 2, 3, 5))) comt " +
+        "RIGHT JOIN (SELECT ts.generate_series AS stime, ts.generate_series + ?3 * INTERVAL '1 second' AS etime " +
+        "FROM (SELECT generate_series(?1, ?2, ?3 * INTERVAL '1 second')) ts) tss " +
+        "ON (comt.PaidTime < tss.etime AND comt.PaidTime >= tss.stime) GROUP BY tss.stime, tss.etime ORDER BY tss.stime", nativeQuery = true)
     List<Object[]> getAvgRevenueTrendWithAllPlatform(Timestamp beginTime, Timestamp endTime, Integer interval);
 
 }
