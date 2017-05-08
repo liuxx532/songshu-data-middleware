e257a80f6ade885f5b98cceb29d48bff815902cb
huanghaizhou <huanghaizhou@co-mall.com>
2017-05-08 19:21:47 +0800

    rSS-4997-松鼠数据分析-渠道页面-后端-渠道页面渠道6大指标联动-黄海洲

--- src/main/java/com/comall/songshu/repository/channel/ChannelRevenueRepository.java
+++ src/main/java/com/comall/songshu/repository/channel/ChannelRevenueRepository.java
@@ -1,5 +1,7 @@
 package com.comall.songshu.repository.channel;
 
+import com.comall.songshu.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Timestamp;
@@ -8,7 +10,7 @@ import java.sql.Timestamp;
  * 渠道销售额
  * Created by huanghaizhou on 2017/5/8.
  */
-public interface ChannelRevenueRepository {
+public interface ChannelRevenueRepository  extends JpaRepository<Author,Long> {
 
     // 所有平台（5 个）
     @Query(value = "SELECT now()", nativeQuery = true)
