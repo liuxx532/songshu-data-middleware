5b4ed3f134cfbebb3eb423cfa6a0136104a5d74e
liushengling <liushengling@co-mall.com>
2017-05-18 19:42:00 +0800

    SS-5123-松鼠数据分析-后端-sql优化-刘胜灵

--- src/main/java/com/comall/songshu/repository/index/CategoryRevenueRankingRepository.java
+++ src/main/java/com/comall/songshu/repository/index/CategoryRevenueRankingRepository.java
@@ -13,33 +13,41 @@ import java.util.List;
  */
 public interface CategoryRevenueRankingRepository extends JpaRepository<Author,Long> {
 
-// SQL:
-// SELECT c."Name",sum(i."AfterFoldingPrice") AS tmount
-// FROM(SELECT o.* FROM songshu_cs_order o
-// JOIN( SELECT DISTINCT "MergePaymentNo" FROM songshu_cs_payment_record
-// WHERE "PaymentModeType" = 2 AND "PaidTime" BETWEEN ? AND ?)r ON o."OrderNumber" = r."MergePaymentNo"
-// JOIN songshu_cs_order_payable p ON o."Id" = p."OrderId" WHERE p."PaymentStatus" = 1 AND o."OrderStatus" NOT IN (6,7) AND o."Channel" IN (0, 1, 2, 3, 5) ) oo
-// LEFT JOIN songshu_cs_order_item i ON oo."Id" = i."OrderId" LEFT JOIN songshu_cs_product p ON i."ProductId" = p."Id"
-// INNER JOIN songshu_cs_category c ON p."CategoryId" = c."Id" WHERE c."Id" != 1 GROUP BY c."Name" ORDER BY tmount DESC
 
+    /**
+     * 销售额品类排行（全平台）
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
     @Query(value = "SELECT c.\"Name\",COALESCE(SUM(i.\"AfterFoldingPrice\"),0) AS tmount " +
-        "FROM(SELECT o.* FROM songshu_cs_order o JOIN( SELECT DISTINCT \"MergePaymentNo\" FROM songshu_cs_payment_record " +
-        "WHERE \"PaymentModeType\" = 2 AND \"PaidTime\" BETWEEN ?1 AND ?2)r " +
-        "ON o.\"OrderNumber\" = r.\"MergePaymentNo\" JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\" " +
-        "WHERE p.\"PaymentStatus\" = 1 AND o.\"OrderStatus\" NOT IN (6,7) AND o.\"Channel\" IN (0, 1, 2, 3, 5) ) oo " +
-        "LEFT JOIN songshu_cs_order_item i ON oo.\"Id\" = i.\"OrderId\" LEFT JOIN songshu_cs_product p ON i.\"ProductId\" = p.\"Id\" " +
-        "INNER JOIN songshu_cs_category c ON p.\"CategoryId\" = c.\"Id\" " +
-        "WHERE c.\"Id\" != 1 GROUP BY c.\"Name\" ORDER BY tmount DESC", nativeQuery = true)
+        "FROM  songshu_cs_category c " +
+        "INNER JOIN songshu_cs_product p ON c.\"Id\" = p.\"CategoryId\" " +
+        "INNER JOIN songshu_cs_order_item i ON i.\"ProductId\" = p.\"Id\" " +
+        "INNER JOIN songshu_cs_order o ON o.\"Id\" = i.\"OrderId\" " +
+        "INNER JOIN songshu_cs_order_payable op ON o.\"Id\" = op.\"OrderId\" " +
+        "INNER JOIN (SELECT \"MergePaymentNo\",MAX(\"PaidTime\") AS paidTime FROM songshu_cs_payment_record WHERE \"PaymentModeType\" =2 " +
+        "            GROUP BY \"MergePaymentNo\",\"PaymentModeType\") r ON op.\"MergePaymentId\" = r.\"MergePaymentNo\" " +
+        "WHERE c.\"Id\" != 1 AND op.\"PaymentStatus\" = 1 AND o.\"OrderStatus\" NOT IN (6,7) AND r.paidTime BETWEEN ?1 AND ?2 " +
+        "GROUP BY c.\"Name\" ORDER BY tmount DESC;", nativeQuery = true)
     List<Object[]> getCategoryRevenueRankingWithAllPlatform(Timestamp beginTime, Timestamp endTime);
 
-
+    /**
+     * 销售额品类排行（单平台）
+     * @param platform
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
     @Query(value = "SELECT c.\"Name\",COALESCE(SUM(i.\"AfterFoldingPrice\"),0) AS tmount " +
-        "FROM(SELECT o.* FROM songshu_cs_order o JOIN( SELECT DISTINCT \"MergePaymentNo\" FROM songshu_cs_payment_record " +
-        "WHERE \"PaymentModeType\" = 2 AND \"PaidTime\" BETWEEN ?2 AND ?3)r " +
-        "ON o.\"OrderNumber\" = r.\"MergePaymentNo\" JOIN songshu_cs_order_payable p ON o.\"Id\" = p.\"OrderId\" " +
-        "WHERE p.\"PaymentStatus\" = 1 AND o.\"OrderStatus\" NOT IN (6,7) AND o.\"Channel\" = ?1 ) oo " +
-        "LEFT JOIN songshu_cs_order_item i ON oo.\"Id\" = i.\"OrderId\" LEFT JOIN songshu_cs_product p ON i.\"ProductId\" = p.\"Id\" " +
-        "INNER JOIN songshu_cs_category c ON p.\"CategoryId\" = c.\"Id\" " +
-        "WHERE c.\"Id\" != 1 GROUP BY c.\"Name\" ORDER BY tmount DESC", nativeQuery = true)
+        "FROM  songshu_cs_category c " +
+        "INNER JOIN songshu_cs_product p ON c.\"Id\" = p.\"CategoryId\" " +
+        "INNER JOIN songshu_cs_order_item i ON i.\"ProductId\" = p.\"Id\" " +
+        "INNER JOIN songshu_cs_order o ON o.\"Id\" = i.\"OrderId\" " +
+        "INNER JOIN songshu_cs_order_payable op ON o.\"Id\" = op.\"OrderId\" " +
+        "INNER JOIN (select \"MergePaymentNo\",MAX(\"PaidTime\") AS paidTime from songshu_cs_payment_record WHERE \"PaymentModeType\" =2 " +
+        "            GROUP BY \"MergePaymentNo\",\"PaymentModeType\") r ON op.\"MergePaymentId\" = r.\"MergePaymentNo\" " +
+        "WHERE c.\"Id\" != 1 AND op.\"PaymentStatus\" = 1 AND o.\"OrderStatus\" NOT IN (6,7) AND o.\"Channel\" = ?1 AND r.paidTime BETWEEN ?2 AND ?3  " +
+        "GROUP BY c.\"Name\" ORDER BY tmount DESC;", nativeQuery = true)
     List<Object[]> getCategoryRevenueRankingWithSinglePlatform(Integer platform, Timestamp beginTime, Timestamp endTime);
 }
